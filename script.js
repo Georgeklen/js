cec5d8bee778fa2dcb1da6bf598e0f43bea89ade
Georgeklen <69301022+Georgeklen@users.noreply.github.com>
2020-08-07 19:42:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let money = 1000;
-let income = ("фриланс");
-let addExpenses = ('Такси, Интернет, Коммуналка.');
-alert (addExpenses);
-let deposit =  (1 == 1) ; // выведится true
+let income = "фриланс";
+let addExpenses = 'Такси, Интернет, Коммуналка.';
+alert(addExpenses);
+let deposit = (1 == 1) ; // выведится true
 let mission = 30000;
 let period = 1;
 console.log(typeof(money));
 console.log(typeof(income));
 console.log (typeof(deposit));
-alert (`Период равен ${period} месяца.`);
-alert (`Цель заработать ${mission}рублей.`);
-console.log (addExpenses.toLowerCase());
-console.log (addExpenses.split (', '))
+alert(`Период равен ${period} месяца.`);
+alert(`Цель заработать ${mission}рублей.`);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split (', '))
 let budgetDay = 3000;
-console.log (budgetDay);
+console.log(budgetDay);
 
 
