41d45491db5a3c67e151a353de200fa1190bd3db
Georgeklen <69301022+Georgeklen@users.noreply.github.com>
2020-08-11 18:45:35 +0700

    two commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // "use strict";
-// let money = prompt("Ваш месячный доход?");
+let money = prompt("Ваш месячный доход?");
 // console.log (money);
 // let addExpenses = prompt ("Перечислите возможные расходы за рассчитываемый период через запятую");
 // console.log (addExpenses.split(",  "));
@@ -10,13 +10,13 @@
 // let expensesTwo = prompt ("Введите обязательную статью расходов?");
 // console.log (expensesOne);
 // console.log (expensesTwo);
-let amountOne = prompt ("Во сколько это обойдется?");
-let amountTwo = prompt ("Во сколько это обойдется?");
+
 // let budgetMonth;
 // budgetMonth = money - (Number (amountOne) + Number (amountTwo));
 // console.log(typeof budgetMonth)
 // console.log(`Бюджет на месяц = ${budgetMonth}`);
-// let mission = 50000;
+let mission;
+mission = 50000;
 // mission = mission / budgetMonth;
 // console.log (Math.ceil(mission));
 // let budgetDay;
@@ -32,8 +32,23 @@ let amountTwo = prompt ("Во сколько это обойдется?");
 //  alert("Что то пошло не так")
 // }
 
-
+let amountOne = prompt ("Во сколько это обойдется?");
+let amountTwo = prompt ("Во сколько это обойдется?");
 let sum  = function () {
- Number(amountOne) + Number(amountTwo);
+ console.log ( Number(amountOne) + Number(amountTwo));
 }
-console.log(sum)
+sum();
+
+let getAccumulatedMonth = function (){
+   console.log ( money - (Number (amountOne) + Number (amountTwo) )) 
+}
+
+getAccumulatedMonth();
+
+
+let accumulatedMonth = getAccumulatedMonth();
+
+let getTargetMonth = function () {
+    console.log  (Number ( mission / getAccumulatedMonth))
+}
+getTargetMonth()
