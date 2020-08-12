ced5e746c08d975fc05b312591798f96aca7c086
Georgeklen <69301022+Georgeklen@users.noreply.github.com>
2020-08-12 23:07:05 +0700

    коммит

--- script.js
+++ script.js
@@ -12,12 +12,7 @@ console.log (expensesOne);
 console.log (expensesTwo);
 let amountOne = prompt ("Во сколько это обойдется?");
 let amountTwo = prompt ("Во сколько это обойдется?");
-let budgetMonth;
-budgetMonth = money - (Number (amountOne) + Number (amountTwo));
-console.log(typeof budgetMonth)
-console.log(`Бюджет на месяц = ${budgetMonth}`);
 let mission = 50000;
-// mission = mission / budgetMonth;
 console.log (Math.ceil(mission));
 let budgetDay;
 
@@ -56,7 +51,8 @@ let getTargetMonth = function () {
 }
 console.log(getStatusIncome())
 
-function showTypeOf(typeOf) {
-    return typeOf;
+let showTypeOf = function(data) {
+    console.log(data, typeof(data));
 }
-console.log(showTypeOf (typeof (accumulatedMonth)))
+
+showTypeOf(money);
