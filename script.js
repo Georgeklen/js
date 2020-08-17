9985295cb62fb7ecc55fe52ab20f596847919184
Georgeklen <69301022+Georgeklen@users.noreply.github.com>
2020-08-17 20:01:53 +0700

    chore: убрал недостатки кода.

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-let money = 1000;
-let income = "фриланс";
+const money = 1000;
+let budgetDay = 3000;
+const income = "фриланс";
 let addExpenses = 'Такси, Интернет, Коммуналка.';
 alert(addExpenses);
 let deposit = (1 == 1) ; // выведится true
-let mission = 30000;
-let period = 1;
-console.log(typeof(money));
-console.log(typeof(income));
-console.log (typeof(deposit));
+const mission = 30000;
+const period = 1;
+console.log(typeof money,typeof income,typeof deposit);
 alert(`Период равен ${period} месяца.`);
-alert(`Цель заработать ${mission}рублей.`);
+alert(`Цель заработать ${mission} рублей.`);
 console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split (', '))
-let budgetDay = 3000;
-console.log(budgetDay);
-
-
+console.log(budgetDay); 
