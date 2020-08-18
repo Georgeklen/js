5b441b5bd07aea4d431a1915473cd1db2b43d165
Georgeklen <69301022+Georgeklen@users.noreply.github.com>
2020-08-18 20:03:33 +0700

    chore:(changed variables)

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const money = 1000;
-let budgetDay = 3000;
+const budgetDay = 3000;
 const income = "фриланс";
-let addExpenses = 'Такси, Интернет, Коммуналка.';
+const addExpenses = 'Такси, Интернет, Коммуналка.';
 alert(addExpenses);
-let deposit = (1 == 1) ; // выведится true
+let deposit = true ; // выведится true
 const mission = 30000;
 const period = 1;
 console.log(typeof money,typeof income,typeof deposit);
